9ecadfd0da253b66c93490193a9a8bf067b5f276
zens <99699841+ZensDK@users.noreply.github.com>
2023-08-07 07:28:46 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-const express = require('express');
-const server = express();
- 
-server.all('/', (req, res) => {
-  res.send(`Free hosting at Uptimerobot.com!`)
-})
- 
-function keepAlive() {
-  server.listen(3000, () => { console.log("Server has initiated" + Date.now()) });
-}
- 
-module.exports = keepAlive;
+from flask import Flask, send_file
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return send_file('Website/index.html')
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
